d4765bec380fd0ca4bf8e946784ff6eae79cfc4f
Muzietto <marco_faustinelli@yahoo.com>
2014-11-07 15:26:15 +0100

    updates to the main comments

--- monad.js
+++ monad.js
@@ -59,7 +59,10 @@
 //                    var scp = monad.run(state),
 //                    nextState = scp[0],
 //                    nextValue = scp[1];
-//                    return func(nextValue).run(nextState);
+//                    return func.apply(
+//                        undefined,
+//                       [nextValue].concat(Array.prototype.slice.apply(args || []))
+//                    ).run(nextState);
 //                });
 //            };
 //            monad.run = function (state) {
@@ -79,6 +82,52 @@
 //            return [state, state];
 //        });
 //    };
+//
+//    var WRITER = MONAD(function(monad, couple) { // couple :: [value, monoid]
+//        // default monoid is string
+//        if (!Array.isArray(couple)) {
+//            couple = [couple, ''];
+//        }
+//        var value = couple[0];
+//        var monoid = couple[1];
+//        var mappend = function(monoid) {
+//            switch (typeof monoid) {
+//                case 'number': 
+//                case 'string': 
+//                    return function(value) { return monoid + value; }
+//                    break;
+//                case 'boolean': // use case for booleans is form validation
+//                    return function(value) { return monoid && value; }
+//                    break;
+//                case 'object': 
+//                    if (Array.isArray(monoid)) { // case 'array'
+//                        return function(value) { return monoid.concat(value); }
+//                        break;
+//                    }
+//                case 'function': // all untested
+//                case 'Sum': 
+//                case 'Prod': 
+//                case 'Any': 
+//                case 'All': 
+//                case 'Ord': 
+//                    return function(value) { return monoid.mappend(value); }
+//            }
+//        }(monoid);
+//        monad.bind = function(fawb) { // fawb = a -> writer b
+//            var newCouple = fawb(value).run();
+//            return writer([newCouple[0], mappend(newCouple[1])]);
+//        }
+//        monad.run = function() {
+//            return couple;
+//        }
+//        monad._1 = function() {
+//            return couple[0];
+//        }();
+//        monad._2 = function() {
+//            return couple[1];
+//        }();
+//        return couple;
+//    });
 
 function MONAD(modifier) {
     'use strict';
